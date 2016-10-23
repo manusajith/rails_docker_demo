a109960f24329d5c508ff59fbbbc92a863b05292
Manu S Ajith <neo@codingarena.in>
2016-10-23 16:56:27 +0530

    Fix error with puma configuration

--- config/puma.rb
+++ config/puma.rb
@@ -4,5 +4,5 @@ threads threads_count, threads_count
 port ENV.fetch('PORT') { 3000 }
 environment ENV.fetch('RAILS_ENV') { 'development' }
 plugin :tmp_restart
-pidfile "#{RAILS_ROOT}/tmp/pids/puma.pid"
+pidfile "tmp/pids/puma.pid"
 workers ENV.fetch('WEB_CONCURRENCY') { 0 }
