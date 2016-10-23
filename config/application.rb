e822e47419a070db86796d6f635adf50a54c0f0c
Manu S Ajith <neo@codingarena.in>
2016-10-23 16:00:34 +0530

    Update application.rb to log output to STDOUT
    Signed-off-by: Manu S Ajith <neo@codingarena.in>

--- config/application.rb
+++ config/application.rb
@@ -11,5 +11,8 @@ module RailsDockerDemo
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
+    config.log_level = :debug
+    config.log_tags  = [:subdomain, :uuid]
+    config.logger    = ActiveSupport::TaggedLogging.new(Logger.new(STDOUT))
   end
 end
