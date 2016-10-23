a1d0df2366ef0e2f12c0709973ed216cf85281e8
Manu S Ajith <neo@codingarena.in>
2016-10-23 16:28:19 +0530

    Use Sidekiq as ActiveJob adapter
    Signed-off-by: Manu S Ajith <neo@codingarena.in>

--- config/application.rb
+++ config/application.rb
@@ -11,9 +11,10 @@ module RailsDockerDemo
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
-    config.log_level   = :debug
-    config.log_tags    = [:subdomain, :uuid]
-    config.logger      = ActiveSupport::TaggedLogging.new(Logger.new(STDOUT))
-    config.cache_store = :redis_store, ENV['CACHE_URL'], { namespace: 'rails_docker_demo::cache' }
+    config.log_level                = :debug
+    config.log_tags                 = [:subdomain, :uuid]
+    config.logger                   = ActiveSupport::TaggedLogging.new(Logger.new(STDOUT))
+    config.cache_store              = :redis_store, ENV['CACHE_URL'], { namespace: 'rails_docker_demo::cache' }
+    config.active_job.queue_adapter = :sidekiq
   end
 end
