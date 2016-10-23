96cb2cb7c36f5c46d5c362ef0453c1040c6a140e
Manu S Ajith <neo@codingarena.in>
2016-10-23 16:16:31 +0530

    Use redis as cache store
    Signed-off-by: Manu S Ajith <neo@codingarena.in>

--- config/application.rb
+++ config/application.rb
@@ -11,8 +11,9 @@ module RailsDockerDemo
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
-    config.log_level = :debug
-    config.log_tags  = [:subdomain, :uuid]
-    config.logger    = ActiveSupport::TaggedLogging.new(Logger.new(STDOUT))
+    config.log_level   = :debug
+    config.log_tags    = [:subdomain, :uuid]
+    config.logger      = ActiveSupport::TaggedLogging.new(Logger.new(STDOUT))
+    config.cache_store = :redis_store, ENV['CACHE_URL'], { namespace: 'rails_docker_demo::cache' }
   end
 end
